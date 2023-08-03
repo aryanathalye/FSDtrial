aa976093866e52786a15008e79f0a40909c9b878
aryanathalye <109202479+aryanathalye@users.noreply.github.com>
2023-08-03 09:55:52 +0530

    Update minmax.py
    this is updated program of max from list

--- minmax.py
+++ minmax.py
@@ -1,3 +1,4 @@
 numbers = {1,7,2,8,0,4,28,90}
 max_value = max(numbers)
-print('Max of list is :', max_value)
+min_value = min(numbers)
+print('Max of list is :', max_value, 'Min of list is :', min_value)
