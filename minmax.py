871e8181d6ed6e07a8eec8b9191c9bb03731892a
aryanathalye <109202479+aryanathalye@users.noreply.github.com>
2023-08-06 23:18:48 +0530

    Update minmax.py
    this is fsdlab1 update in branch1

--- minmax.py
+++ minmax.py
@@ -2,3 +2,4 @@ numbers = {1,7,2,8,0,4,28,90}
 max_value = max(numbers)
 min_value = min(numbers)
 print('Max of list is :', max_value, 'Min of list is :', min_value)
+print("This is the modified version of the branches")
